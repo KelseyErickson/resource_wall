093fcc3131fa7913afd1a784b60a82ccfc7756e0
Asim <asim.awan@outlook.com>
2021-10-23 21:24:04 +0000

    Add login route

--- server.js
+++ server.js
@@ -52,6 +52,11 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/login", (req, res) => {
+  const templateVars = { user: activeUser([req.session.user_id], users) };
+  res.render("urls_login", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
