29e0990aeb078a951c319808e5c54e2d78147990
Kelsey Erickson <kerickson@gmx.com>
2021-10-28 15:20:57 +0000

    Add registration functionailty

--- routes/registration.js
+++ routes/registration.js
@@ -8,11 +8,52 @@
 const express = require('express');
 const router  = express.Router();
 
-module.exports = () => {
+module.exports = (db) => {
   router.get("/", (req, res) => {
-  // const templateVars = { user: activeUser([req.session.user_id], users) };
-  // res.render("login", templateVars);
-  res.render("registration");
+    const user_idCookie = req.session.user_id;
+
+  // const templateVars = {
+  //   userInfo: users[user_idCookie],
+  //   message: null
+  // };
+
+
+    res.render("registration");
   });
+
+router.post('/', (req, res) => {
+
+    const name = req.body.name
+    const email = req.body.email;
+    const password = req.body.password;
+    const profilePic = req.body.profilePic;
+
+    if (!email || !password || !name) {
+      res.redirect('/registration')
+
+    }
+
+
+    let query = `INSERT INTO users (name, email, password, photo_url)
+    values ($1, $2, $3, $4);`;
+
+    let params = [name, email, password, profilePic];
+    console.log(query, params)
+    db.query(query, params)
+      .then(data => {
+
+
+        req.session.user_id = data.rows.id;
+
+        res.redirect('/')
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+
+  });
+
   return router;
 };
